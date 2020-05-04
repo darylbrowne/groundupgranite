a3c009d9fddc5a93a8dcc7d0c701bda707813cb4
Daryl Browne <darylbrowne@post.harvard.edu>
2020-05-04 13:42:25 -0400

    Update block.test.js

--- tests/block.test.js
+++ tests/block.test.js
@@ -1,8 +1,8 @@
 // block.test.js
 const Person = require('../js/block');
-test('Block constructs with a first and last name', () => {
+test('Block constructs with a model and actual JSON', () => {
   let testBlock = new Block('modelJSON', 'actualJSON')
-  expect(testBlock).toEqual({model: 'modelJSON', actual: 'actualJSON'});
+  expect(testBlock).toEqual({modelJSON: 'modelJSON', actualJSON: 'actualJSON'});
 });
 test('fullJSON returns Block instance of combnined JSON', () => {
   let testBlock = new Block('modelJSON', 'actualJSON')
